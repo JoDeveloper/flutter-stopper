5cd59c55425740ffef43fcf23ec7b718f7302062
joseph Omer <jodeveloper8@gmail.com>
2022-07-27 08:42:35 +0300

    migrate to null-saftey

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,79 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:stopper/stopper.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello")),
+      appBar: AppBar(title: const Text("Hello")),
       body: Builder(
         builder: (context) {
           final h = MediaQuery.of(context).size.height;
           return Center(
-            child: MaterialButton(
-              color: Colors.green,
-              child: Text("Show Stopper"),
-              onPressed: () {
-                showStopper(
-                  context: context,
-                  stops: [0.4 * h, h],
-                  builder: (context, scrollController, scrollPhysics, stop) {
-                    return ClipRRect(
-                      borderRadius: stop == 0 ? BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ): BorderRadius.only(),
-                      clipBehavior: Clip.antiAlias,
-                      child: Container(
-                        color: Colors.orange,
-                        child: CustomScrollView(
-                          slivers: <Widget>[
-                            SliverAppBar(
-                              title: Text("What's Up?"),
-                              backgroundColor: Colors.orange,
-                              automaticallyImplyLeading: false,
-                              primary: false,
-                              floating: true,
-                              pinned: true,
-                            ),
-                            SliverList(
-                              delegate: SliverChildBuilderDelegate(
-                                (context, idx) => ListTile(
-                                    title: Text("Nothing much"),
-                                    subtitle: Text("$idx"),
-                                ),
-                                childCount: 100,
+              child: MaterialButton(
+            color: Colors.green,
+            child: const Text("Show Stopper"),
+            onPressed: () {
+              showStopper(
+                context: context,
+                stops: [0.4 * h, h],
+                builder: (context, scrollController, scrollPhysics, stop) {
+                  return ClipRRect(
+                    borderRadius: stop == 0
+                        ? const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          )
+                        : const BorderRadius.only(),
+                    clipBehavior: Clip.antiAlias,
+                    child: Container(
+                      color: Colors.orange,
+                      child: CustomScrollView(
+                        slivers: <Widget>[
+                          const SliverAppBar(
+                            title: Text("What's Up?"),
+                            backgroundColor: Colors.orange,
+                            automaticallyImplyLeading: false,
+                            primary: false,
+                            floating: true,
+                            pinned: true,
+                          ),
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, idx) => ListTile(
+                                title: const Text("Nothing much"),
+                                subtitle: Text("$idx"),
                               ),
-                            )
-                          ],
-                          controller: scrollController,
-                          physics: scrollPhysics,
-                        ),
+                              childCount: 100,
+                            ),
+                          )
+                        ],
+                        controller: scrollController,
+                        physics: scrollPhysics,
                       ),
-                    );
-                  },
-                );
-              },
-            )
-          );
+                    ),
+                  );
+                },
+              );
+            },
+          ));
         },
       ),
     );
--- lib/stopper.dart
+++ lib/stopper.dart
@@ -1,46 +1,46 @@
 library stopper;
 
-import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+
 /// A builder function to be passed to [Stopper].
-typedef Widget StopperBuilder(
-    /// A build context
-    BuildContext context,
-    /// A scroll controller to be passed to a scrollable widget
-    ScrollController controller, 
-    // A scroll physics to be passed to a scrollable widget
-    ScrollPhysics physics, 
-    /// The current stop value.
-    int stop
-  );
+typedef StopperBuilder = Widget Function(
+  /// A build context
+  BuildContext context,
+
+  /// A scroll controller to be passed to a scrollable widget
+  ScrollController? controller,
+  // A scroll physics to be passed to a scrollable widget
+  ScrollPhysics? physics,
+
+  /// The current stop value.
+  int? stop,
+);
 
 /// A widget that changes its height to one of the predefined values based on user-initiated dragging.
 /// Designed to be used with [showBottomSheet()] method.
 class Stopper extends StatefulWidget {
-    /// The list of stop heights in logical pixels. The values must be sorted from lowest to highest.
+  /// The list of stop heights in logical pixels. The values must be sorted from lowest to highest.
   final List<double> stops;
-    /// This callback function is called when the user triggers a close. If null, the bottom sheet cannot be closed by the user.
-  final Function onClose;
-    /// A builder to build the contents of the bottom sheet.
+
+  /// This callback function is called when the user triggers a close. If null, the bottom sheet cannot be closed by the user.
+  final Function? onClose;
+
+  /// A builder to build the contents of the bottom sheet.
   final StopperBuilder builder;
-    /// The initial stop.
+
+  /// The initial stop.
   final int initialStop;
-    /// The minimum offset (in logical pixels) necessary to trigger a stop change when dragging.
+
+  /// The minimum offset (in logical pixels) necessary to trigger a stop change when dragging.
   final double dragThreshold;
-    /// THe desidered shape
-  final ShapeBorder shape;
+
+  /// THe desidered shape
+  final ShapeBorder? shape;
 
   /// The constructor.
-  Stopper({
-    Key key,
-    @required this.builder,
-    @required this.stops,
-    this.initialStop = 0,
-    this.onClose,
-    this.dragThreshold = 25,
-    this.shape = null
-  })
+  const Stopper({Key? key, required this.builder, required this.stops, this.initialStop = 0, this.onClose, this.dragThreshold = 25, this.shape})
       : assert(initialStop < stops.length),
         super(key: key);
 
@@ -50,45 +50,46 @@ class Stopper extends StatefulWidget {
 
 /// The state of [Stopper] widget.
 class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
-  List<double> _stops;
-  int _currentStop;
-  int _targetStop;
+  late List<double> _stops;
+  int? _currentStop;
+  int? _targetStop;
   bool _dragging = false;
   bool _closing = false;
-  double _dragOffset;
-  double _closingHeight;
-  ScrollController _scrollController;
-  ScrollPhysics _scrollPhysics;
-  Animation<double> _animation;
-  AnimationController _animationController;
-  Tween<double> _tween;
+  late double _dragOffset;
+  double? _closingHeight;
+  ScrollController? _scrollController;
+  ScrollPhysics? _scrollPhysics;
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+  late Tween<double> _tween;
 
   ScrollPhysics _getScrollPhysicsForStop(s) {
-    if (s == _stops.length - 1)
-      return BouncingScrollPhysics();
-    else
-      return NeverScrollableScrollPhysics();
+    if (s == _stops.length - 1) {
+      return const BouncingScrollPhysics();
+    } else {
+      return const NeverScrollableScrollPhysics();
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    this._stops = widget.stops;
-    this._currentStop = widget.initialStop;
-    this._targetStop = _currentStop;
+    _stops = widget.stops;
+    _currentStop = widget.initialStop;
+    _targetStop = _currentStop;
     _scrollController = ScrollController();
     _scrollPhysics = _getScrollPhysicsForStop(_currentStop);
-    _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
     final curveAnimation = CurvedAnimation(parent: _animationController, curve: Curves.linear);
-    _tween = Tween<double>(begin: _stops[_currentStop], end: _stops[_targetStop]);
+    _tween = Tween<double>(begin: _stops[_currentStop!], end: _stops[_targetStop!]);
     _animation = _tween.animate(curveAnimation);
-    _scrollController.addListener(() {
-      if (_scrollController.offset < -widget.dragThreshold) {
-        if (this._currentStop != this._targetStop || _dragging) return;
-        if (this._currentStop > 0) {
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset < -widget.dragThreshold) {
+        if (_currentStop != _targetStop || _dragging) return;
+        if (_currentStop! > 0) {
           final h0 = height;
-          this._targetStop = this._currentStop - 1;
-          _animate(h0, _stops[_targetStop]);
+          _targetStop = _currentStop! - 1;
+          _animate(h0, _stops[_targetStop!]);
         } else if (!_closing) {
           close();
         }
@@ -99,16 +100,16 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
   @override
   void didUpdateWidget(Stopper oldWidget) {
     super.didUpdateWidget(oldWidget);
-    this._stops = widget.stops;
-    this._currentStop = min(_currentStop, _stops.length - 1);
-    this._targetStop = min(_currentStop, _stops.length - 1);
+    _stops = widget.stops;
+    _currentStop = min(_currentStop!, _stops.length - 1);
+    _targetStop = min(_currentStop!, _stops.length - 1);
   }
 
   @override
   void dispose() {
     super.dispose();
     _animationController.dispose();
-    _scrollController.dispose();
+    _scrollController!.dispose();
   }
 
   /// The current stop value. The value changes after the stop change animation is complete.
@@ -131,17 +132,17 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
       _animationController.stop(canceled: true);
       _dragging = false;
       _closing = true;
-      widget.onClose();
+      widget.onClose!();
     }
   }
 
-  void _animate(double from, double to, [double velocity]) {
+  void _animate(double? from, double to, [double? velocity]) {
     _tween.begin = from;
     _tween.end = to;
     _animationController.value = 0;
-    if (_scrollController.offset < 0) _scrollController.animateTo(0, duration: Duration(milliseconds: 200), curve: Curves.linear);
+    if (_scrollController!.offset < 0) _scrollController!.animateTo(0, duration: const Duration(milliseconds: 200), curve: Curves.linear);
     _animationController.fling().then((_) {
-      this._currentStop = this._targetStop;
+      _currentStop = _targetStop;
       setState(() {
         _scrollPhysics = _getScrollPhysicsForStop(_currentStop);
       });
@@ -150,14 +151,15 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
 
   /// The current height of the bottom sheet.
   get height {
-    if (_closing)
+    if (_closing) {
       return _closingHeight;
-    else if (_dragging)
-      return _stops[_currentStop] + _dragOffset;
-    else if (_animationController.isAnimating)
+    } else if (_dragging) {
+      return _stops[_currentStop!] + _dragOffset;
+    } else if (_animationController.isAnimating) {
       return _animation.value;
-    else
-      return _stops[_currentStop];
+    } else {
+      return _stops[_currentStop!];
+    }
   }
 
   @override
@@ -167,14 +169,14 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
         child: GestureDetector(
           onVerticalDragStart: (details) {
             if (_currentStop != _targetStop) return;
-            _scrollController.jumpTo(0);
+            _scrollController!.jumpTo(0);
             _dragging = true;
             _dragOffset = 0;
             setState(() {});
           },
           onVerticalDragUpdate: (details) {
             if (_dragging) {
-              _scrollController.jumpTo(0);
+              _scrollController!.jumpTo(0);
               _dragOffset -= details.delta.dy;
               setState(() {});
             }
@@ -182,15 +184,15 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
           onVerticalDragEnd: (details) {
             if (!_dragging || _closing) return;
             if (_dragOffset > widget.dragThreshold) {
-              _targetStop = min(_currentStop + 1, _stops.length - 1);
+              _targetStop = min(_currentStop! + 1, _stops.length - 1);
             } else if (_dragOffset < -widget.dragThreshold) {
-              _targetStop = max(canClose ? -1 : 0, _currentStop - 1);
+              _targetStop = max(canClose ? -1 : 0, _currentStop! - 1);
             }
-            if (_targetStop < 0) {
+            if (_targetStop! < 0) {
               close();
             } else {
               _dragging = false;
-              _animate(_stops[_currentStop] + _dragOffset, _stops[_targetStop]);
+              _animate(_stops[_currentStop!] + _dragOffset, _stops[_targetStop!]);
             }
           },
           child: widget.builder(context, _scrollController, _scrollPhysics, _currentStop),
@@ -205,30 +207,38 @@ class StopperState extends State<Stopper> with SingleTickerProviderStateMixin {
 }
 
 /// Shows the Stopper bottom sheet.
-/// Returns a [PersistentBottomSheetController] that can be used 
+/// Returns a [PersistentBottomSheetController] that can be used
 PersistentBottomSheetController showStopper(
     {
+
     /// The key of the [Stopper] widget
-    Key key,
-    /// The build context 
-    @required BuildContext context,
+    Key? key,
+
+    /// The build context
+    required BuildContext context,
+
     /// The builder of the bottom sheet
-    @required StopperBuilder builder,
+    required StopperBuilder builder,
+
     /// The list of stop heights as logical pixel values. Use [MediaQuery] to compute the heights relative to screen height.
     /// The order of the stop heights must be from the lowest to the highest.
-    @required List<double> stops,
+    required List<double> stops,
+
     /// The initial stop number.
     int initialStop = 0,
-    /// If [true] then the user can close the bottom sheet dragging it down from the lowest stop. 
+
+    /// If [true] then the user can close the bottom sheet dragging it down from the lowest stop.
     bool userCanClose = true,
+
     /// The minimum offset (in logical pixels) to trigger a stop change when dragging.
     double dragThreshold = 25,
+
     /// The desidered shape
-    ShapeBorder shape = null,
+    ShapeBorder? shape,
+
     /// Required for modal bottomSheet
-    bool isScrollController = true
-    }) {
-  PersistentBottomSheetController cont;
+    bool isScrollController = true}) {
+  late PersistentBottomSheetController cont;
   cont = showBottomSheet(
     context: context,
     builder: (context) {
@@ -239,9 +249,11 @@ PersistentBottomSheetController showStopper(
         stops: stops,
         initialStop: initialStop,
         dragThreshold: dragThreshold,
-        onClose: userCanClose ? () {
-          cont.close();
-        }: null,
+        onClose: userCanClose
+            ? () {
+                cont.close();
+              }
+            : null,
       );
     },
   );
@@ -250,28 +262,35 @@ PersistentBottomSheetController showStopper(
 
 /// Shows the Stopper modal bottom sheet.
 /// Returns a [Future] that can be used
-Future showModalStopper(
-    {
-      /// The key of the [Stopper] widget
-      Key key,
-      /// The build context
-      @required BuildContext context,
-      /// The builder of the bottom sheet
-      @required StopperBuilder builder,
-      /// The list of stop heights as logical pixel values. Use [MediaQuery] to compute the heights relative to screen height.
-      /// The order of the stop heights must be from the lowest to the highest.
-      @required List<double> stops,
-      /// The initial stop number.
-      int initialStop = 0,
-      /// If [true] then the user can close the bottom sheet dragging it down from the lowest stop.
-      bool userCanClose = true,
-      /// The minimum offset (in logical pixels) to trigger a stop change when dragging.
-      double dragThreshold = 25,
-      /// The desidered shape
-      ShapeBorder shape = null,
-      /// Required for modal bottomSheet
-      bool isScrollController = true,
-    }) {
+Future showModalStopper({
+  /// The key of the [Stopper] widget
+  Key? key,
+
+  /// The build context
+  required BuildContext context,
+
+  /// The builder of the bottom sheet
+  required StopperBuilder builder,
+
+  /// The list of stop heights as logical pixel values. Use [MediaQuery] to compute the heights relative to screen height.
+  /// The order of the stop heights must be from the lowest to the highest.
+  required List<double> stops,
+
+  /// The initial stop number.
+  int initialStop = 0,
+
+  /// If [true] then the user can close the bottom sheet dragging it down from the lowest stop.
+  bool userCanClose = true,
+
+  /// The minimum offset (in logical pixels) to trigger a stop change when dragging.
+  double dragThreshold = 25,
+
+  /// The desidered shape
+  ShapeBorder? shape,
+
+  /// Required for modal bottomSheet
+  bool isScrollController = true,
+}) {
   Future cont;
 
   cont = showModalBottomSheet(
